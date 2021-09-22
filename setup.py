44c643e12db2adf584300922323fa9777b389b90
v33rh0ra <tanveer.hora@gmail.com>
2021-09-22 14:17:05 +0530

    init

--- setup.py
+++ setup.py
@@ -3,15 +3,18 @@ print("Packages found:",find_packages())
 
 setup(
     name = "get_nse_daily",
-    version="1.0.0",
+    version="0.1",
     packages=find_packages(),
-    author="tanveer.hora@gmail.com",
-    description="",
-    keywords = "",
+    author="Tanveer Hora",
+    author_email = 'tanveer.hora@gmail.com',
+    license='MIT',
+    description="pure python library built using requests to get daily day end data from nse india",
+    keywords = "nse,bhavcopy,bhav",
     install_requires = [
         'requests>=2.26.0'
     ],
-    url = "",#project home page from github
+    url = "https://github.com/v33rh0ra/get_nse_daily",#project home page from github
+    download_url = 'https://github.com/v33rh0ra/get_nse_daily/archive/v_01.tar.gz',
     project_urls={
         "Documentation":"",
         "Bug Tracker":"",
@@ -19,11 +22,12 @@ setup(
     },
     classifiers=[
         "Operating System :: OS Independent",
-        "Programming Language :: Python",
+        'Programming Language :: Python :: 3'
         "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering",
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
-        "Intended Audience :: Science/Research"
+        'Intended Audience :: Developers',
     ],
 )
